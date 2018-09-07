2fd23499d8cf4e73627379820fbb9e8d3cbb8634
Rob Trew <houthakker72@gmail.com>
2018-09-07 14:36:59 +0100

    Restored simpler iterate
    (preferring explicit functions on tuples to relying on a magic term counter in the generator)

--- JS Prelude MD/iterate.md
+++ JS Prelude MD/iterate.md
@@ -2,11 +2,9 @@
 // iterate :: (a -> a) -> a -> Generator [a]
 function* iterate(f, x) {
     let v = x;
-    let i = 1;
     while (true) {
         yield(v);
-        v = f(v, i);
-        i = 1 + i;
+        v = f(v);
     }
 }
 ```
--- jsPrelude.js
+++ jsPrelude.js
@@ -1254,11 +1254,9 @@ const iso8601Local = dte =>
 // iterate :: (a -> a) -> a -> Generator [a]
 function* iterate(f, x) {
     let v = x;
-    let i = 1;
     while (true) {
         yield(v);
-        v = f(v, i);
-        i = 1 + i;
+        v = f(v);
     }
 }
 
