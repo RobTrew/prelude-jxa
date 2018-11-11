ee1e11d00abe8df7905f5f8ce727f2440bf7adb8
Rob Trew <houthakker72@gmail.com>
2018-11-11 16:31:35 +0000

    Updated permutations
    Replaced recursion with a fold

--- jsPrelude.js
+++ jsPrelude.js
@@ -1863,16 +1863,31 @@ const partitionEithers = xs =>
     );
 
 // permutations :: [a] -> [[a]]
-const permutations = xs => {
-    const go = xs =>
-        xs.length ? concatMap(x => concatMap(ys => [
-                [x].concat(ys)
-            ],
-            go(delete_(x, xs))), xs) : [
-            []
-        ];
-    return go(xs);
-};
+const permutations = xs =>
+    xs.reduceRight(
+        (a, x) => concatMap(
+            xs => enumFromTo(0, xs.length)
+            .map(n => xs.slice(0, n)
+                .concat(x)
+                .concat(xs.slice(n))
+            ),
+            a
+        ),
+        [[]]
+    );
+
+// OR
+// // permutations :: [a] -> [[a]]
+// const permutations = xs => {
+//     const go = xs =>
+//         xs.length ? concatMap(x => concatMap(ys => [
+//                 [x].concat(ys)
+//             ],
+//             go(delete_(x, xs))), xs) : [
+//             []
+//         ];
+//     return go(xs);
+// };
 
 // permutationsWithRepetition :: Int -> [a] -> [[a]]
 const permutationsWithRepetition = (n, xs) =>
