d632ced327880b5f140c566b5e448040d345979c
Rob Trew <houthakker72@gmail.com>
2020-01-12 04:47:58 +0000

    findIndexR updated
     (just comment)

--- JS Prelude MD/findIndexR.md
+++ JS Prelude MD/findIndexR.md
@@ -1,6 +1,9 @@
 ```js
 // findIndexR :: (a -> Bool) -> [a] -> Maybe Int
 const findIndexR = p =>
+    //  Just the index of the last element in
+    //  xs for which p(x) is true, or 
+    //  Nothing if there is no such element.
     xs => {
         const i = reverse('string' !== typeof xs ? (
             xs
--- jsPrelude.js
+++ jsPrelude.js
@@ -1141,6 +1141,9 @@ const findIndex = p =>
 
 // findIndexR :: (a -> Bool) -> [a] -> Maybe Int
 const findIndexR = p =>
+    //  Just the index of the last element in
+    //  xs for which p(x) is true, or 
+    //  Nothing if there is no such element.
     xs => {
         const i = reverse('string' !== typeof xs ? (
             xs
