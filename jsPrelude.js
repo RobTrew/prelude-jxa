5959e7c5d9be76d2144acdfee4bde36dec79dfb5
Rob Trew <houthakker72@gmail.com>
2020-01-13 19:55:19 +0000

    Updated curryN

--- jsPrelude.js
+++ jsPrelude.js
@@ -504,18 +504,15 @@ const curry = f =>
 // curryN :: ((a, b) -> c) -> a -> b -> c
 const curryN = f =>
     // Flexibly handles two or more arguments, applying
-    // the function directly (if the argument array
+    // the function directly (if the argument list
     // is long enough for complete saturation),
     // or recursing with a concatenation of any existing and
     // newly supplied arguments, while gaps remain.
     (...args) => {
         const
-            n = f.length,
-            go = xs => n <= xs.length ? (
+            go = xs => f.length <= xs.length ? (
                 f(...xs)
-            ) : function() {
-                return go(xs.concat(Array.from(arguments)));
-            };
+            ) : (...ys) => go([...xs, ...ys]);
         return go(args);
     };
 
