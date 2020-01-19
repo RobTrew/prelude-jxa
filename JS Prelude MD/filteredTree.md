a005c51d732be2ae8e08a11c03753486a4f381c7
Rob Trew <houthakker72@gmail.com>
2020-01-19 20:57:54 +0000

    Adjusted filteredTree

--- JS Prelude MD/filteredTree.md
+++ JS Prelude MD/filteredTree.md
@@ -1,8 +1,8 @@
 ```js
-// filteredTree (a - Bool) -> Tree a -> Tree a
+// filteredTree (a -> Bool) -> Tree a -> Tree a
 const filteredTree = p =>
-    // A tree including only those children which:
-    // either match the predicate p, or have
+    // A tree including only those children
+    // which either match the predicate p, or have
     // descendants which match the predicate p.
     foldTree(x => xs =>
         Node(x)(xs.filter(
--- jsPrelude.js
+++ jsPrelude.js
@@ -1114,10 +1114,10 @@ const filterTree = p =>
         concat(p(x) ? [x, ...xs] : xs)
     )
 
-// filteredTree (a - Bool) -> Tree a -> Tree a
+// filteredTree (a -> Bool) -> Tree a -> Tree a
 const filteredTree = p =>
-    // A tree including only those children which:
-    // either match the predicate p, or have
+    // A tree including only those children
+    // which either match the predicate p, or have
     // descendants which match the predicate p.
     foldTree(x => xs =>
         Node(x)(xs.filter(
