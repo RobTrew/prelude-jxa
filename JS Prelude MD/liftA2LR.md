ea548c6f87a248d9e0eea698f76f3f85ce34fd2c
Rob Trew <houthakker72@gmail.com>
2020-04-18 10:07:51 +0100

    Updated comment to liftA2LR

--- JS Prelude MD/liftA2LR.md
+++ JS Prelude MD/liftA2LR.md
@@ -1,6 +1,8 @@
 ```js
 // liftA2LR :: (a -> b -> c) -> Either d a -> Either d b -> Either d c
 const liftA2LR = f =>
+    // The binary function f lifted to a
+    // function over two Either values.
     a => b => bindLR(a)(
         x => bindLR(b)(
             compose(Right, f(x))
--- jsPrelude.js
+++ jsPrelude.js
@@ -2015,6 +2015,8 @@ const liftA2Fn = op => f => g =>
 
 // liftA2LR :: (a -> b -> c) -> Either d a -> Either d b -> Either d c
 const liftA2LR = f =>
+    // The binary function f lifted to a
+    // function over two Either values.
     a => b => bindLR(a)(
         x => bindLR(b)(
             compose(Right, f(x))
