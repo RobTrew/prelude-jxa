5bed55f0083be8b27f508dad83149eb94dc06815
Rob Trew <houthakker72@gmail.com>
2020-04-18 10:50:28 +0100

    Updated docstring for apLR

--- JS Prelude MD/apLR (   ).md
+++ JS Prelude MD/apLR (   ).md
@@ -1,7 +1,7 @@
 ```js
 // apLR (<*>) :: Either e (a -> b) -> Either e a -> Either e b
 const apLR = flr =>
-  // Either application of a possible function in Either
-  // to a possible value in Either, or a Left value.
+  // Either a Left value, or the application of a
+  // function in Either to a value in Either.
   liftA2LR(identity)(flr)
 ```
--- jsPrelude.js
+++ jsPrelude.js
@@ -152,8 +152,8 @@ const apFn = f =>
 
 // apLR (<*>) :: Either e (a -> b) -> Either e a -> Either e b
 const apLR = flr =>
-  // Either application of a possible function in Either
-  // to a possible value in Either, or a Left value.
+  // Either a Left value, or the application of a
+  // function in Either to a value in Either.
   liftA2LR(identity)(flr)
 
 // apList (<*>) :: [(a -> b)] -> [a] -> [b]
