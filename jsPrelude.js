fa4330914683e6068cf18082ae5eb4d23bb6a37a
Rob Trew <houthakker72@gmail.com>
2020-05-01 08:36:35 +0100

    uncurry
    Branch on odd argument count.
    
    (Simple curryable functions will have 2 arguments, .reduce and .reduceRight contexts will supply 4)

--- jsPrelude.js
+++ jsPrelude.js
@@ -4223,10 +4223,11 @@ const uncurry = f =>
     // A function over a pair, derived
     // from a curried function.
     function() {
-        const args = Array.from(arguments);
-        const xy = [2, 4].includes(args.length) ? (
-            args
-        ) : args[0];
+        const
+            args = arguments,
+            xy = Boolean(args.length % 2) ? (
+                args[0]
+            ) : args;
         return f(xy[0])(xy[1]);
     };
 
