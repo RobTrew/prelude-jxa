752bbc9ee2bc7f5ebb65dd0c155edcbb8622d29f
Rob Trew <houthakker72@gmail.com>
2020-07-13 00:27:50 +0100

    Updated partition

--- jsPrelude.js
+++ jsPrelude.js
@@ -2788,8 +2788,7 @@ const partition = p =>
     // A tuple of two lists - those elements in 
     // xs which match p, and those which don't.
     xs => list(xs).reduce(
-        (a, x) =>
-        p(x) ? (
+        (a, x) => p(x) ? (
             Tuple(a[0].concat(x))(a[1])
         ) : Tuple(a[0])(a[1].concat(x)),
         Tuple([])([])
