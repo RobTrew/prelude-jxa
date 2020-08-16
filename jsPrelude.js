44ed7e23b3a789071fd9c3d70517ffbbb261a5da
Rob Trew <houthakker72@gmail.com>
2020-08-16 23:13:39 +0100

    Added drafts of merge, mergeBy

--- jsPrelude.js
+++ jsPrelude.js
@@ -2676,6 +2676,33 @@ const measuredTree = tree => {
 // member :: Key -> Dict -> Bool
 const member = k => dct => k in dct;
 
+// merge :: Ord a => [a] -> [a] -> [a]
+const merge = xs =>
+    // An ordered list derived by merging
+    // two other ordered lists.
+    mergeBy(compare)(xs);
+
+// mergeBy :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
+const mergeBy = f =>
+    // A single list defined by the ordered 
+    // merging of xs and ys in terms of the 
+    // given comparator function.
+    xs => ys => {
+        const go = (as, bs) =>
+            0 < bs.length ? (
+                0 < as.length ? (
+                    1 !== f(as[0])(bs[0]) ? (
+                        [as[0]].concat(
+                            go(as.slice(1), bs)
+                        )
+                    ) : [bs[0]].concat(
+                        go(as, bs.slice(1))
+                    )
+                ) : bs
+            ) : as;
+        return [].concat(...go(xs, ys));
+    };
+
 // min :: Ord a => a -> a -> a
 const min = a => b => b < a ? b : a;
 
