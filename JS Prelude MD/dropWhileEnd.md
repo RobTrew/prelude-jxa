c982735324b874a30e2c044214cc04803818c9bc
Rob Trew <houthakker72@gmail.com>
2020-08-17 10:34:07 +0100

    Comment edits to dropWhileEnd

--- JS Prelude MD/dropWhileEnd.md
+++ JS Prelude MD/dropWhileEnd.md
@@ -1,7 +1,9 @@
 ```js
 // dropWhileEnd :: (a -> Bool) -> [a] -> [a]
-// dropWhileEnd :: (Char -> Bool) -> String -> String
+// dropWhileEnd :: (Char -> Bool) -> String -> [Char]
 const dropWhileEnd = p =>
+    // xs without the longest suffix for which
+    // p returns true for all elements.
     xs => {
         let i = xs.length;
         while (i-- && p(xs[i])) {}
--- jsPrelude.js
+++ jsPrelude.js
@@ -984,8 +984,10 @@ const dropWhile = p =>
     };
 
 // dropWhileEnd :: (a -> Bool) -> [a] -> [a]
-// dropWhileEnd :: (Char -> Bool) -> String -> String
+// dropWhileEnd :: (Char -> Bool) -> String -> [Char]
 const dropWhileEnd = p =>
+    // xs without the longest suffix for which
+    // p returns true for all elements.
     xs => {
         let i = xs.length;
         while (i-- && p(xs[i])) {}
