817dd0705a0737f7a2e4523cbdee8e87aa3fb809
Rob Trew <houthakker72@gmail.com>
2020-09-12 19:47:24 +0100

    Simplified headMay

--- jsPrelude.js
+++ jsPrelude.js
@@ -1742,11 +1742,12 @@ const headDef = v =>
     ) : v;
 
 // headMay :: [a] -> Maybe a
-const headMay = xs => (
-    ys => 0 < ys.length ? (
-        Just(ys[0])
-    ) : Nothing()
-)(list(xs));
+const headMay = xs =>
+    // Just the first item of xs, or
+    // Nothing if xs is an empty list.
+    0 < xs.length ? (
+        Just(xs[0])
+    ) : Nothing();
 
 // identity :: a -> a
 const identity = x =>
