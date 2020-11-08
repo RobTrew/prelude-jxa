33a07abc442b183f6b3d9bcbf7a8d121f83461a7
Rob Trew <houthakker72@gmail.com>
2020-11-08 22:27:42 +0000

    updated traverse

--- MD javascript vs applescript/traverse.md
+++ MD javascript vs applescript/traverse.md
@@ -26,8 +26,12 @@ end traverse
 ```javascript
 // traverse :: (Applicative f, Traversable t) => 
 // (a -> f b) -> t a -> f (t b)
-const traverse = f => tx =>
-    ({
+const traverse = f =>
+    // Each element of a structure mapped to an 
+    // a functor-wrapped value, with evaluation from
+    // from left to right, and the results collected
+    // in a single instance of the target functor.
+    tx => ({
         'Either': () => traverseLR,
         'Maybe': () => traverseMay,
         'Node': () => traverseTree,
--- jsPrelude.js
+++ jsPrelude.js
@@ -4197,8 +4197,12 @@ const transpose_ = rows =>
 
 // traverse :: (Applicative f, Traversable t) => 
 // (a -> f b) -> t a -> f (t b)
-const traverse = f => tx =>
-    ({
+const traverse = f =>
+    // Each element of a structure mapped to an 
+    // a functor-wrapped value, with evaluation from
+    // from left to right, and the results collected
+    // in a single instance of the target functor.
+    tx => ({
         'Either': () => traverseLR,
         'Maybe': () => traverseMay,
         'Node': () => traverseTree,
