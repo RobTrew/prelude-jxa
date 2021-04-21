ff8df752225315548d3692105fd87c7b6c540bce
Rob Trew <houthakker72@gmail.com>
2021-04-21 14:38:14 +0100

    Updated gcd

--- MD javascript vs applescript/gcd.md
+++ MD javascript vs applescript/gcd.md
@@ -3,12 +3,12 @@
 const gcd = x =>
     y => {
         const zero = x.constructor(0);
-        const _gcd = (a, b) =>
+        const go = (a, b) =>
             zero === b ? (
                 a
-            ) : _gcd(b, rem(a)(b));
+            ) : go(b, rem(a)(b));
 
-        return _gcd(abs(x), abs(y));
+        return go(abs(x), abs(y));
     };
 ```
 
--- jsPrelude.js
+++ jsPrelude.js
@@ -1754,12 +1754,12 @@ const ft = m =>
 const gcd = x =>
     y => {
         const zero = x.constructor(0);
-        const _gcd = (a, b) =>
+        const go = (a, b) =>
             zero === b ? (
                 a
-            ) : _gcd(b, rem(a)(b));
+            ) : go(b, rem(a)(b));
 
-        return _gcd(abs(x), abs(y));
+        return go(abs(x), abs(y));
     };
 
 // genericIndexMay :: [a] -> Int -> Maybe a
