6409a02f04ad6ff4253323be357437063b028d8a
Rob Trew <houthakker72@gmail.com>
2021-10-24 19:35:21 +0100

    Updated zipWithN

--- jsPrelude.js
+++ jsPrelude.js
@@ -5366,15 +5366,16 @@ const zipWithM = f =>
 // zipWithN :: (a -> b -> ... -> c) -> ([a], [b] ...) -> [c]
 const zipWithN = (...args) => {
     const
-        rows = args.slice(1).map(list),
-        f = compose(uncurryN(args[0]), TupleN),
-        n = Math.min(...rows.map(x => x.length));
+        rows = args.slice(1).map(xs => Array.from(xs)),
+        n = Math.min(...rows.map(x => x.length)),
+        f = uncurryN(args[0]);
 
     return 0 < n ? (
-        take(n))(rows[0]).map(
-        (x, i) => f(rows.flatMap(
-            v => v[i]
-        ))
+        take(n)(rows[0]).map(
+            (x, i) => f(TupleN(
+                ...rows.flatMap(v => v[i])
+            ))
+        )
     ) : [];
 };
 
