d525007663e949a58f53e626904ee0d913d7f45a
Rob Trew <houthakker72@gmail.com>
2021-12-11 12:15:03 +0000

    Updated compareList

--- jsPrelude.js
+++ jsPrelude.js
@@ -590,12 +590,10 @@ const compareList = xs =>
     // 0 if two lists are identical.
     // -1 if xs is empty, or has a lower leftward value.
     // 1 if ys is empty, or has a lower leftward value.
-    ys => compare(
-        Number(0 === xs.length)
-    )(
-        Number(0 === ys.length)
-    ) || compare(xs[0])(ys[0]) || (
-        compareList(xs.slice(1))(ys.slice(1))
+    ys => compare(0 === xs.length)(0 === ys.length) || (
+        compare(xs[0])(ys[0]) || (
+            compareList(xs.slice(1))(ys.slice(1))
+        )
     );
 
 // comparing :: (a -> b) -> (a -> a -> Ordering)
