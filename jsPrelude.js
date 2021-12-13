588481a28207fec40170efc5e507ac56e367d322
Rob Trew <houthakker72@gmail.com>
2021-12-13 21:52:04 +0000

    Added forestFromJSONLR

--- jsPrelude.js
+++ jsPrelude.js
@@ -1803,6 +1803,23 @@ const foldrTree = f =>
         return go(acc, node);
     };
 
+// forestFromJSONLR :: JSON String -> Either String Forest a
+const forestFromJSONLR = json => {
+    // Either a message string or a Forest.
+    // Assumes a recursive [root, nest] JSON format,
+    // in which `root` is a parseable value string,
+    // and `nest` is a possibly empty list of
+    // [`root`, `nest`] pairs.
+    const go = vxs =>
+        Node(vxs[0])(vxs[1].map(go));
+
+    return bindLR(
+        jsonParseLR(json)
+    )(
+        xs => Right(xs.map(go))
+    );
+};
+
 // fromEnum :: Enum a => a -> Int
 const fromEnum = x =>
     typeof x !== "string" ? (
