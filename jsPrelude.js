40b0e897b648c727ce14e22ec4cfdf5831460911
Rob Trew <houthakker72@gmail.com>
2022-04-29 15:38:15 +0100

    Moved combinations function into general Prelude

--- jsPrelude.js
+++ jsPrelude.js
@@ -610,6 +610,24 @@ const chunksOf = n => {
     return go;
 };
 
+// combinations :: Int -> [a] -> [[a]]
+const combinations = n =>
+    // All combinations, without repetition,
+    // of n items drawn from xs.
+    xs => {
+        const go = (m, ys) =>
+            1 > m ? [
+                []
+            ] : 0 === ys.length ? (
+                []
+            ) : ((h, tail) => go(m - 1, tail)
+                .map(t => [h].concat(t))
+                .concat(go(m, tail))
+            )(ys[0], ys.slice(1));
+
+        return (go)(n, xs);
+    };
+
 // combine (</>) :: FilePath -> FilePath -> FilePath
 const combine = fp =>
     // Two paths combined with a path separator.
