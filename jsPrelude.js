a8657ba9256e9dc38ef09b8763d080c078b8db95
Rob Trew <houthakker72@gmail.com>
2022-06-18 19:10:00 +0100

    Added example to iterateUntil

--- jsPrelude.js
+++ jsPrelude.js
@@ -2449,8 +2449,10 @@ const iterate = f =>
 // iterateUntil :: (a -> Bool) -> (a -> a) -> a -> [a]
 const iterateUntil = p =>
     // Like `until`, but returns a list of
-    // all intermediate values, where until
+    // intermediate values, where until
     // returns only a final value.
+    // iterateUntil(x => 5 < x)(x => 2 * x)(1)
+    // -> [1, 2, 4, 8]
     f => x => {
         const
             go = v => p(v) ? (
