67772561f67bec776501be56ce26c51a72d04991
Rob Trew <houthakker72@gmail.com>
2022-06-23 18:38:39 +0100

    fmapLR updated

--- MD javascript vs applescript/fmapLR (<$>).md
+++ MD javascript vs applescript/fmapLR (<$>).md
@@ -1,9 +1,11 @@
 ```javascript
-// fmapLR (<$>) :: (a -> b) -> Either a a -> Either a b
-const fmapLR = f => lr =>
-    "Left" in lr ? (
-        lr
-    ) : Right(f(lr.Right));
+// fmapLR (<$>) :: (b -> c) -> Either a b -> Either a c
+const fmapLR = f =>
+    // Either f mapped into the contents of any Right
+    // value in e, or e unchanged if is a Left value.
+    e => "Left" in e ? (
+        e
+    ) : Right(f(e.Right));
 ```
 
 
--- jsPrelude.js
+++ jsPrelude.js
@@ -1736,11 +1736,13 @@ const fmapGen = f =>
         }
     };
 
-// fmapLR (<$>) :: (a -> b) -> Either a a -> Either a b
-const fmapLR = f => lr =>
-    "Left" in lr ? (
-        lr
-    ) : Right(f(lr.Right));
+// fmapLR (<$>) :: (b -> c) -> Either a b -> Either a c
+const fmapLR = f =>
+    // Either f mapped into the contents of any Right
+    // value in e, or e unchanged if is a Left value.
+    e => "Left" in e ? (
+        e
+    ) : Right(f(e.Right));
 
 // fmapMay (<$>) :: (a -> b) -> Maybe a -> Maybe b
 const fmapMay = f => mb =>
