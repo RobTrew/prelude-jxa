16d96343bded6092ccc39167c516a757d065d8ce
Rob Trew <houthakker72@gmail.com>
2022-07-08 23:38:32 +0100

    Updated comment in eqSet

--- JS Prelude MD/eqSet.md
+++ JS Prelude MD/eqSet.md
@@ -2,7 +2,7 @@
 // eqSet :: Set a -> Set a -> Bool
 const eqSet = a =>
     // True if the two sets have
-    // the same set of members.
+    // the same size and members.
     b => a.size === b.size && (
         Array.from(a).every(x => b.has(x))
     );
--- jsPrelude.js
+++ jsPrelude.js
@@ -1397,7 +1397,7 @@ const eqDateTime = n =>
 // eqSet :: Set a -> Set a -> Bool
 const eqSet = a =>
     // True if the two sets have
-    // the same set of members.
+    // the same size and members.
     b => a.size === b.size && (
         Array.from(a).every(x => b.has(x))
     );
