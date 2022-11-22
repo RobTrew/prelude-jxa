1394ddff23fa38aed4bc1e659101e97d8d4a14c2
Rob Trew <houthakker72@gmail.com>
2022-11-22 18:57:31 +0000

    Updated groupOnKey

--- jsPrelude.js
+++ jsPrelude.js
@@ -2117,13 +2117,15 @@ const groupOnKey = f => {
     // A list of (k, [a]) tuples, in which each [a]
     // contains only elements for which f returns the
     // same value, and in which k is that value.
+    // The concatenation of the [a] in each tuple === xs.
     const go = xs =>
         0 < xs.length ? (() => {
             const
                 x = xs[0],
-                t = xs.slice(1),
                 fx = f(x),
-                [yes, no] = span(y => fx === f(y))(t);
+                [yes, no] = span(y => fx === f(y))(
+                    xs.slice(1)
+                );
 
             return [[fx, [x, ...yes]]].concat(go(no));
         })() : [];
