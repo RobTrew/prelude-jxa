f7fdd2b7d7da2867acf8977319ad3720b4774108
Rob Trew <houthakker72@gmail.com>
2024-04-22 21:05:16 +0100

    Tidied nest

--- jsPrelude.js
+++ jsPrelude.js
@@ -3544,10 +3544,10 @@ const negate = n =>
 
 // nest :: Tree a -> [a]
 const nest = tree => {
-// Allowing for lazy (on-demand) evaluation.
-// If the nest turns out to be a function –
-// rather than a list – that function is applied
-// here to the root, and returns a list.
+    // Allowing for lazy (on-demand) evaluation.
+    // If the nest turns out to be a function –
+    // rather than a list – that function is applied
+    // here to the root, and returns a list.
     const xs = tree.nest;
 
     return "function" !== typeof xs
