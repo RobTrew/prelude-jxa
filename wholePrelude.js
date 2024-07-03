46f1ccad3bdf4c7184da83b3e586d5bc41bf0a06
Rob Trew <houthakker72@gmail.com>
2024-07-03 21:30:45 +0100

    Minor name change in zipWithN

--- wholePrelude.js
+++ wholePrelude.js
@@ -6504,18 +6504,18 @@ const zipWithM = f =>
 const zipWithN = (...args) => {
     // Uncurried function of which the first argument is
     // a function, and all remaining arguments are lists.
-    const go = ([f0, ...vs]) => {
+    const go = ([f0, ...rows]) => {
         const
             n = Math.min(
-                ...vs.map(x => x.length)
+                ...rows.map(x => x.length)
             );
 
-        return vs.slice(1).reduce(
+        return rows.slice(1).reduce(
             (fs, xs) => fs.map(
                 (f, i) => f(xs[i])
             )
             .slice(0, n),
-            vs[0].map(f0)
+            rows[0].map(f0)
         );
     };
 
