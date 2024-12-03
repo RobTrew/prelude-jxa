0f2707fefb6cc583016331ff2febd09b7cf2891d
Rob Trew <houthakker72@gmail.com>
2024-12-03 11:39:07 +0000

    zipWithN linting

--- wholePrelude.js
+++ wholePrelude.js
@@ -6534,7 +6534,6 @@ const zipWithN = (f, ...xss) => {
     // Generalisation of ZipWith, ZipWith3 etc.
     // f is a curried function absorbing at least 
     // N arguments, where is the length of xss.
-    // xss => {
     const m = 0 < xss.length
         ? Math.min(...xss.map(x => x.length))
         : 0;
