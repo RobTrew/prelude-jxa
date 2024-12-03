d9ca4e12750ad0750eaf4135509aa2fecd1734ab
Rob Trew <houthakker72@gmail.com>
2024-12-03 11:36:01 +0000

    Adjusted the type of zipWithN ( now uncurried )
    (Arbitrary number of list arguments)

--- JS Prelude MD/zipWithN.md
+++ JS Prelude MD/zipWithN.md
@@ -1,17 +1,17 @@
 ```javascript
-// zipWithN :: (a -> b -> ... -> c) -> [[a], [b] ...] -> [c]
-const zipWithN = f =>
+// zipWithN :: (a -> b -> ... -> d) -> [a], [b] ... -> [d]
+const zipWithN = (f, ...xss) => {
     // Generalisation of ZipWith, ZipWith3 etc.
     // f is a curried function absorbing at least 
-    // N arguments, where N is the length of xss.
-    xss => {
-        const m = 0 < xss.length
-            ? Math.min(...xss.map(x => x.length))
-            : 0;
+    // N arguments, where is the length of xss.
+    // xss => {
+    const m = 0 < xss.length
+        ? Math.min(...xss.map(x => x.length))
+        : 0;
 
-        return xss.reduce(
-            (gs, vs) => gs.map((g, i) => g(vs[i])),
-            Array.from({ length: m }, () => f)
-        );
-    };
+    return xss.reduce(
+        (gs, vs) => gs.map((g, i) => g(vs[i])),
+        Array.from({ length: m }, () => f)
+    );
+};
 ```
--- wholePrelude.js
+++ wholePrelude.js
@@ -6529,21 +6529,21 @@ const zipWithM = f =>
             )([...ys])
         );
 
-// zipWithN :: (a -> b -> ... -> c) -> [[a], [b] ...] -> [c]
-const zipWithN = f =>
+// zipWithN :: (a -> b -> ... -> d) -> [a], [b] ... -> [d]
+const zipWithN = (f, ...xss) => {
     // Generalisation of ZipWith, ZipWith3 etc.
     // f is a curried function absorbing at least 
-    // N arguments, where N is the length of xss.
-    xss => {
-        const m = 0 < xss.length
-            ? Math.min(...xss.map(x => x.length))
-            : 0;
+    // N arguments, where is the length of xss.
+    // xss => {
+    const m = 0 < xss.length
+        ? Math.min(...xss.map(x => x.length))
+        : 0;
 
-        return xss.reduce(
-            (gs, vs) => gs.map((g, i) => g(vs[i])),
-            Array.from({ length: m }, () => f)
-        );
-    };
+    return xss.reduce(
+        (gs, vs) => gs.map((g, i) => g(vs[i])),
+        Array.from({ length: m }, () => f)
+    );
+};
 
 // zipWith_ :: (a -> a -> b) -> [a] -> [b]
 const zipWith_ = f =>
