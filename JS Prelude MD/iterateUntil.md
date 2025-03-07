b5689c1416d767c5f6d3c09f0ad1aaab76988658
Rob Trew <houthakker72@gmail.com>
2025-03-07 19:31:00 +0000

    Switched implementation of iterateUntil to iterative
    (Recursive implementation was constrained by stack size)

--- JS Prelude MD/iterateUntil.md
+++ JS Prelude MD/iterateUntil.md
@@ -1,17 +1,20 @@
 ```javascript
 // iterateUntil :: (a -> Bool) -> (a -> a) -> a -> [a]
 const iterateUntil = p =>
-    // Like `until`, but returns a list of
-    // intermediate values, where until
-    // returns only a final value.
-    // iterateUntil(x => 5 < x)(x => 2 * x)(1)
-    // -> [1, 2, 4, 8]
+    // The value resulting from successive applications
+    // of f to f(x), starting with a seed value x,
+    // and terminating when the result returns true
+    // for the predicate p.
     f => x => {
-        const
-            go = v => p(v)
-                ? [v]
-                : [v, ...go(f(v))];
+        let v = x;
 
-        return go(x);
+        const xs = [];
+
+        while (!p(v)) {
+            v = f(v);
+            xs.push(v);
+        }
+
+        return xs;
     };
 ```
--- wholePrelude.js
+++ wholePrelude.js
@@ -2936,18 +2936,21 @@ const iterate = f =>
 
 // iterateUntil :: (a -> Bool) -> (a -> a) -> a -> [a]
 const iterateUntil = p =>
-    // Like `until`, but returns a list of
-    // intermediate values, where until
-    // returns only a final value.
-    // iterateUntil(x => 5 < x)(x => 2 * x)(1)
-    // -> [1, 2, 4, 8]
+    // The value resulting from successive applications
+    // of f to f(x), starting with a seed value x,
+    // and terminating when the result returns true
+    // for the predicate p.
     f => x => {
-        const
-            go = v => p(v)
-                ? [v]
-                : [v, ...go(f(v))];
+        let v = x;
 
-        return go(x);
+        const xs = [];
+
+        while (!p(v)) {
+            v = f(v);
+            xs.push(v);
+        }
+
+        return xs;
     };
 
 // iterateUntilGen :: (a -> Bool) -> (a -> a) ->
