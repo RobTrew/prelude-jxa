38f8df3ca27d934bd30f37003ead4cf8c57a86b4
Rob Trew <houthakker72@gmail.com>
2025-11-07 11:25:23 +0000

    Adjusted some tree measurement functions
    treeHeight, treeWidth and lengthTree
    
    (fractionally more performant)

--- jsPrelude.js
+++ jsPrelude.js
@@ -2896,8 +2896,15 @@ const length = xs =>
 // lengthTree :: Tree a -> Int
 const lengthTree = tree => {
     // The number of nodes in the tree.
-    const go = (n, t) =>
-        n + nest(t).reduce(go, 1);
+    const go = (n, t) => {
+        const xs = nest(t);
+
+        return n + (
+            0 < xs.length
+                ? xs.reduce(go, 1)
+                : 1
+        );
+    };
 
     return go(0, tree);
 };
